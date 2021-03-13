26b634f6b1ef9382d77df17e3125afddddb9e568
Alex <frstfng@gmail.com>
2021-03-13 18:57:35 +0300

    Completed

--- main.c
+++ main.c
@@ -1,7 +1,28 @@
 #include <stdio.h>
 
-int main()
-{
-  printf("Hello World!\n");
+int isPrime(int a) {
+  int primeCheck = 0;
+
+  for (int i = 1; i <= a; i++) {
+      if (!(a % i)) {
+          primeCheck++;
+        }
+      if (primeCheck == 3) {
+          return 1;
+        }
+    }
+  return 0;
+}
+
+int main() {
+  int a = 10;
+
+  do {
+      printf("Введите целое число: ");
+      scanf("%d", &a);
+    }
+  while (a <= 1);
+
+  printf("Число %d %sпростое\n", a, (isPrime(a)? "не ": ""));
   return 0;
 }
